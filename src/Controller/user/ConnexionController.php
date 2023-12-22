e217bb73af828809ef3c06a03f0ec63f30b75d40
Dorian Danilet <danilet.dorian@hotmail.fr>
2023-12-22 12:12:55 +0100

    connexion, ajout du formulaire en html, à refaire sur twig

---
+++ src/Controller/user/ConnexionController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller\user;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConnexionController extends AbstractController
+{
+    #[Route('/connexion', name: 'connexion')]
+    public function index(): Response
+    {
+        return $this->render('user/connexion.html.twig');
+    }
+}
