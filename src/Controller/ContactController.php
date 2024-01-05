1f842c2b04c7317b4f4bc4779b751b798106545e
Sajid Salhi <sajid.s35400@gmail.com>
2024-01-05 17:48:45 +0100

    reste le css a faire

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class ContactController extends AbstractController
+{
+#[Route('/contactez-nous', name: 'app_contactez-nous')]
+public function index(): Response 
+{
+    $formContact= $this->createForm(ContactType::class);
+   return $this->render('contactez-nous.html.twig',['formContact'=>$formContact]);
+}
+
+}
