5693f2786e3a68df67553526deaf2c4ec4094c30
Sajid Salhi <sajid.s35400@gmail.com>
2024-01-09 11:09:56 +0100

    En attente que la page connexion soit faite

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,20 +1,27 @@
 <?php
 namespace App\Controller;
 use App\Entity\User;
+use App\Form\ProfileUserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\DependencyInjection\Loader\Configurator\form;
-
-
 
 class UserController extends AbstractController
 {
 #[Route('/mon_profil', name:'user_profil')]
-    public function index(): Response 
+    public function index(UserInterface $user, Request $request, EntityManagerInterface $entityManager ): Response 
     {
-      
-       /* $formUser -> $this -> createForm(User::class);*/
-       return $this->render('/mon_profil.html.twig');
+      $form=  $this->createForm(ProfileUserType::class, $user);
+      $form->handleRequest($request);
+      if( $form->isSubmitted() && $form->isValid())
+      { 
+      $entityManager->flush();
+      $this->addFlash("success", "votre modification a bien été prise en compte");
+      }
+       return $this->render('/mon_profil.html.twig',["form"=> $form]);
     }
 }
