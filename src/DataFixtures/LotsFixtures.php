1b21b2c85f90bd76e00e92c7a7b1b3769f920309
stephanie meyer <stephanie2510@gmail.com>
2024-01-10 17:37:42 +0100

    fix css

--- src/DataFixtures/LotsFixtures.php
+++ src/DataFixtures/LotsFixtures.php
@@ -9,23 +9,25 @@ use Doctrine\Persistence\ObjectManager;
 class LotsFixtures extends Fixture
 {
     public const TITLES = [
-        'Porte clés',
-        'Pins',
-        'Mug',
-        'Casquette',
-        'Peluches',
-        'Lampe',
-        'Mini borne d\'arcade',
-        'Réveil',
-        'POP',
-        'Clavier',
-        'Souris',
-        'Casque',
-        'Tapis de souris XL',
-        'PS5',
-        'Xbox série X',
-        'Smartphone',
-        'PC portable',
+        ['title' => 'Porte clés', 'image' => 'porte-cle.jpg'],
+        ['title' => 'Pins', 'image' => 'pins.webp'],
+        ['title' => 'Mug', 'image' => 'mug.jpg'],
+        ['title' => 'Casquette', 'image' => 'casquette.jpg'],
+        ['title' => 'Peluches', 'image' => 'peluches.jpg'],
+        ['title' => 'Lampe', 'image' => 'lampe-manette-xbox2.jpg'],
+        ['title' => 'Mini borne d\'arcade', 'image' => 'mini_borne_arcade.jpg'],
+        ['title' => 'Réveil', 'image' => 'reveil-manette-ps1.jpg'],
+        ['title' => 'POP', 'image' => 'POP-h.jpg'],
+        ['title' => 'POP', 'image' => 'POP-c.jpg'],
+        ['title' => 'POP', 'image' => 'POP-s.jpg'],
+        ['title' => 'Clavier', 'image' => 'clavier1.webp'],
+        ['title' => 'Souris', 'image' => 'souris1.jpg'],
+        ['title' => 'Casque', 'image' => 'casque-gamer.jpg'],
+        ['title' => 'Tapis de souris XL', 'image' => 'tapis-de-souris.jpg'],
+        ['title' => 'PS5', 'image' => 'ps5.jpg'],
+        ['title' => 'Xbox série X', 'image' => 'Xbox.jpg'],
+        ['title' => 'Smartphone', 'image' => 'smartphone.webp'],
+        ['title' => 'PC portable', 'image' => 'pc.jpg'],
     ];
 
     public const PRICES = [
@@ -38,12 +40,22 @@ class LotsFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        foreach (self::TITLES as $object) {
+        $uploadLotDir = '/uploads/lot';
+        if (!is_dir(__DIR__ . '/../../public' . $uploadLotDir)) {
+            mkdir(__DIR__ . '/../../public' . $uploadLotDir, recursive: true);
+        }
+
+        foreach (self::TITLES as $gift) {
+            copy(
+                __DIR__ . '/data/lot/' . $gift['image'],
+                __DIR__ . '/../../public' . $uploadLotDir . '/' . $gift['image']
+            );
             $lot = new Lot();
-            $lot->setTitle($object);
+            $lot->setTitle($gift['title']);
             $lot->setPrice(self::PRICES[
                 rand(0, count(self::PRICES) - 1)
                 ]);
+            $lot->setImage($gift['image']);
 
             $manager->persist($lot);
         }
