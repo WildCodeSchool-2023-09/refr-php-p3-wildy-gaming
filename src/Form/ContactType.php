ef8c6184f52a3ee512e2ebae9c9ff8a2424639e4
Sajid Salhi <sajid.s35400@gmail.com>
2024-01-19 14:01:31 +0100

    ok

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -11,7 +11,7 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('pseudo')
+            ->add('field_name')
             ->add('email')
             ->add('message')
         ;
