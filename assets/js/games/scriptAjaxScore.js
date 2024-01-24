4232cfff94cd2bcda2ccb35cbe5b5fdfacc66861
Kynuak <bapt.trouille@gmail.com>
2024-01-24 19:58:34 +0100

    Remise en forme du snake pour template jeu

--- assets/js/games/scriptAjaxScore.js
+++ assets/js/games/scriptAjaxScore.js
@@ -1,7 +1,7 @@
 export function saveScore(score, nameGame) {
 
     const requete = new XMLHttpRequest();
-    requete.open('GET', '/saveScore?score='+score+'&name_game='+nameGame, true)
+    requete.open('GET', '/games/saveScore?score='+score+'&name_game='+nameGame, true)
     requete.onreadystatechange = function() {
         if(requete.readyState === 4 && requete.status === 200) {
             const response = JSON.parse(requete.responseText);
--- src/Controller/GamesController.php
+++ src/Controller/GamesController.php
@@ -36,28 +36,7 @@ class GamesController extends AbstractController
         ]);
     }
 
-    #[Route('/{gameSlug}', methods:['GET'], name:'show')]
-    public function showGame(
-        #[MapEntity(mapping: ['gameSlug' => 'slug'])] Game $game,
-        PlayRepository $playRepository,
-    ): Response {
-
-        $plays = $playRepository->findBy(["game" => $game->getId()], ["score" => "DESC"]);
-        $bestUsers = [];
-
-        foreach ($plays as $play) {
-            if (!in_array($play->getUser()->getUsername(), $bestUsers) && (count($bestUsers) < 3)) {
-                $bestUsers[] = $play->getUser()->getUsername();
-            }
-        }
-
-        return $this->render('games/show.html.twig', [
-            'game' => $game,
-            "bestUsers" => $bestUsers,
-            "plays" => $plays
-        ]);
-    }
-
+    #[Route("/saveScore")]
     public function saveScore(
         Request $request,
         EntityManagerInterface $entityManager,
@@ -82,12 +61,25 @@ class GamesController extends AbstractController
         return new JsonResponse(['success' => true, "score" => $score, "name_game" => $nameGame, "game" => $game]);
     }
 
-    #[Route('/snake', name: 'games_show', methods: ['GET'])]
-    public function show(GameRepository $gameRepository): Response
-    {
-        $games = $gameRepository->findAll();
+    #[Route('/{gameSlug}', methods:['GET'], name:'show')]
+    public function showGame(
+        #[MapEntity(mapping: ['gameSlug' => 'slug'])] Game $game,
+        PlayRepository $playRepository,
+    ): Response {
 
-        return $this->render('games/snake.html.twig', [
-            'games' => $games,]);
+        $plays = $playRepository->findBy(["game" => $game->getId()], ["score" => "DESC"]);
+        $bestUsers = [];
+
+        foreach ($plays as $play) {
+            if (!in_array($play->getUser()->getUsername(), $bestUsers) && (count($bestUsers) < 3)) {
+                $bestUsers[] = $play->getUser()->getUsername();
+            }
+        }
+
+        return $this->render('games/show.html.twig', [
+            'game' => $game,
+            "bestUsers" => $bestUsers,
+            "plays" => $plays
+        ]);
     }
 }
