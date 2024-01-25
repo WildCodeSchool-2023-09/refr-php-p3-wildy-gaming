11a6068b9805ae496d2bfa3cd95d8c7d38971ca8
Dorian Danilet <danilet.dorian@hotmail.fr>
2024-01-25 09:59:04 +0100

    css page contactez-nous

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -5,15 +5,26 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('email')
-            ->add('propo')
+            ->add('name', TextType::class, [
+                'attr' => ['placeholder' => 'Name'],
+                'label' => false 
+            ])
+            ->add('email', EmailType::class, [
+                'attr' => ['placeholder' => 'Email'],
+                'label' => false
+            ])
+            ->add('propo', TextType::class, [
+                'attr' => ['placeholder' => 'Propo'],
+                'label' => false
+            ])
         ;
     }
 
