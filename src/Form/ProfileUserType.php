5d6aac75414c6121aeab0af9295ba8a903ed87ae
Dorian Danilet <danilet.dorian@hotmail.fr>
2024-01-29 10:55:05 +0100

    test css profil

--- src/Form/ProfileUserType.php
+++ src/Form/ProfileUserType.php
@@ -5,6 +5,9 @@ namespace App\Form;
 use App\Entity\Lot;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,18 +17,27 @@ class ProfileUserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class, [
-                'attr' => ['placeholder' => 'Name'],
+            ->add('firstname', TextType::class, [
+                'attr' => ['placeholder' => 'Firstname', 'class' => 'monprofil'],
                 'label' => false
             ])
+            ->add('lastname', TextType::class, [
+                'attr' => ['placeholder' => 'Lastame', 'class' => 'monprofil'],
+                'label' => false
+            ])
+            ->add('username', TextType::class, [
+                'attr' => ['placeholder' => 'username', 'class' => 'monprofil'],
+                'label' => false
+            ])
+            ->add('birthdate', DateType::class, [
+                'label' => false
+            ])
+            
             ->add('email', EmailType::class, [
-                'attr' => ['placeholder' => 'Email'],
+                'attr' => ['placeholder' => 'Email', 'class' => 'monprofil'],
                 'label' => false
             ])
-            ->add('propo', TextType::class, [
-                'attr' => ['placeholder' => 'Propo'],
-                'label' => false
-            ])
+            
         ;
     }
 
